612ff99edc68cf0fa14c09eeb81cc168b7523060
Mathis <mathislegamin@gmail.com>
2025-03-10 08:48:05 +0100

    c cool

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import connectDB from "./Config/database.js";
-import router from "./routers/vaccin.router.js";
+import router from "./routers/ambassade.router.js";
 
 const app = express();
 
 app.use(express.json());
 connectDB();
 
-app.use("/api/vaccins" , router);
+app.use("/api/ambassade" , router);
 
 
 app.listen(3000, () => console.log("Server listen on http://localhost:3000"));
