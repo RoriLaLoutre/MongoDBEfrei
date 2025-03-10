9c28875aee49c92b0934088632d2e9167943977f
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-03-10 09:07:41 +0100

    ajout du get pour 20 lignes

--- Controllers/ambassy.controller.js
+++ Controllers/ambassy.controller.js
@@ -2,21 +2,32 @@ import mongoose from "mongoose";
 import Ambassy from "./Models/ambassy.model.js";
 
 export async function addAmbassy(nom, email) {
-  try {
-    const newAmbassy = new Ambassy({ nom, email });
-    await newAmbassy.save();
-    console.log(`✅ Ambassade ajoutée : ${nom} (${email})`);
-  } catch (error) {
-    console.error("❌ Erreur :", error.message);
-  } finally {
-    mongoose.connection.close();
-  }
+
 }
 
 
 export async function getAllDiplomat() {
   try {
     const allDiplomat = await Ambassy.find(req, res);
+    res.status(200).json(allDiplomat);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export async function getAllDiplomat() {
+  try {
+    const allDiplomat = await Ambassy.find(req, res);
+    res.status(200).json(allDiplomat);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export async function get20Diplomat() {
+  try {
+    const allDiplomat = await Ambassy.find(req, res).limit(20);
     res.status(200).json(allDiplomat);
   } catch (error) {
     res.status(500).json({ message: error.message });
