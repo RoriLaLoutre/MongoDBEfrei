f6f2a5b7342420776528878e72f2526170bc07fe
Russias <russiasesteban@gmail.com>
2025-03-10 11:45:43 +0100

    getbyid getbycity getbyiso

--- Controllers/ambassy.controller.js
+++ Controllers/ambassy.controller.js
@@ -25,3 +25,33 @@ export async function get20Diplomat(req, res, next) {
   }
 };
 
+export async function getid(req, res, next) {
+  const id = req.params.id;
+  try {
+    const getid = await Ambassy.find({ _id: id });
+    res.status(200).json(getid);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export async function getBYiso(req, res, next) {
+  const iso2 = req.params.iso2;
+  try {
+    const getiso2 = await Ambassy.find({ iso2: iso2 });
+    res.status(200).json(getiso2);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export async function getBYcity(req, res, next) {
+  const city = req.params.city;
+  try {
+    const getcity = await Ambassy.find({ ville: city });
+    res.status(200).json(getcity);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
