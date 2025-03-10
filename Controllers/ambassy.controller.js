d66b5f0fe2c3cf34bec0be1fca78b55cd2c1ee69
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-03-10 12:01:20 +0100

    ajout route et debut du crud create

--- Controllers/ambassy.controller.js
+++ Controllers/ambassy.controller.js
@@ -25,3 +25,24 @@ export async function get20Diplomat(req, res, next) {
   }
 };
 
+export async function addAmbassy(req, res, next) {
+  const ambassy = new Ambassy({    // a faire plus tard
+    pays: req.body.pays,
+    type: req.body.type,
+    nom: req.body.nom,
+    adresse: req.body.adresse,
+    ville: req.body.ville,
+    code_postal: req.body.code_postal,
+    telephone: req.body.telephone,
+    fax: req.body.fax,
+    courriel: req.body.courriel,
+    url: req.body.url,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    socials: req.body.socials,
+    iso2: req.body.iso2,
+    date_ajout: req.body.date_ajout,
+  });
+    
+};
+
