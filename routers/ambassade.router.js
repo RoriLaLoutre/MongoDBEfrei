4a833c8648200758621cb377cb4c65de994a6285
Robin Gillot <robin2.gillot2@gmail.com>
2025-03-12 13:43:28 +0100

    first step form

--- routers/ambassade.router.js
+++ routers/ambassade.router.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {AmbassyController} from "../Controllers/index.controller.js";
+import path from 'path';
+
 
 
 const router = express.Router();
@@ -9,7 +11,11 @@ router.get("/all20", AmbassyController.get20Diplomat);
 //router.post("/add", AmbassyController.addAmbassy);
 //router.put("/update/:id", AmbassyController.updateAmbassy);
 //router.delete("/delete/:id", AmbassyController.deleteAmbassy);
-
+// Route pour afficher le formulaire
+router.get('/add', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'add_form.html'),
+    AmbassyController.addAmbassy);
+    });
 router.get("/:id", AmbassyController.getid);
 
 router.get("/search/iso/:iso2", AmbassyController.getBYiso);
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import connectDB from "./Config/database.js";
 import router from "./routers/ambassade.router.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 connectDB();
 
 app.use("/api/ambassade" , router);
